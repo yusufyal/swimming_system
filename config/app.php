f88d05cb13e55feedec87882e6c1b1375db56d88
yusufyal <yniyala@gmail.com>
2025-03-28 16:39:56 +0300

    Added CI/CD configuration and environment files

--- config/app.php
+++ config/app.php
@@ -174,7 +174,6 @@ return [
         Spatie\Permission\PermissionServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
         Milon\Barcode\BarcodeServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
     ])->toArray(),
 
@@ -190,13 +189,9 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-
         'DNS1D' => Milon\Barcode\Facades\DNS1DFacade::class,
         'DNS2D' => Milon\Barcode\Facades\DNS2DFacade::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-
-
     ])->toArray(),
 
 
