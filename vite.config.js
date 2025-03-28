f2d40f4bf7ca41d3ff83317159b477b0ea165bfe
yusufyal <yniyala@gmail.com>
2025-03-28 21:59:56 +0300

    Updated Vite config and rebuilt assets

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            publicDirectory: 'public',
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        assetsDir: 'assets',
+        manifest: true,
+    },
 });
